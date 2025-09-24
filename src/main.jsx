38d56fb91e7ce4429e5593c9aeed70bd850af8f2
Nina <ninasepulvedacarvalho@gmail.com>
2025-09-23 22:32:56 -0300

    Alteração Css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import './fanta.css'
 import App from './App.jsx'
-
+import '/fanta.css'
+import '/index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />

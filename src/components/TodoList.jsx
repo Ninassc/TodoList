cd53552de6265d03a92c783b15a9cb2706721f3c
Nina <ninasepulvedacarvalho@gmail.com>
2025-09-25 16:29:49 -0300

    Correção index

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -15,7 +15,7 @@ export function TodoList(props) {
                     <TodoCard 
                     key={todoIndex} 
                     {...props}
-                    todoIndex={todoIndex}
+                    todoIndex={todos.findIndex(val => val.input == todo.input)}
                     todo = { todo }/>
                 )
                 
